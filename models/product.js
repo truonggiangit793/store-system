62010fd6978ef0101ed21cfe70c780d0cdb56ba5
truonggiangit793 <truonggiangit793@truonggiangit793.local>
2022-10-23 16:36:20 +0700

    Change type of product money from Number to String

--- models/product.js
+++ models/product.js
@@ -29,7 +29,7 @@ const productSchema = new mongoose.Schema(
             required: true,
         },
         inMoney: {
-            type: Number,
+            type: String,
             required: true,
         },
     },
