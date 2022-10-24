bba8980380edbc1f585a5b71ebf815f09960560e
truonggiangit793 <truonggiangit793@truonggiangit793-macbook.local>
2022-10-25 00:25:05 +0700

    Config husky

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
-require('dotenv').config();
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const swaggerAutogen = require('swagger-autogen')();
+require("dotenv").config();
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const swaggerAutogen = require("swagger-autogen")();
 
-const defineRoute = require('./routes/index');
-const db = require('./configs/database');
+const defineRoute = require("./routes/index");
+const db = require("./configs/database");
 
 const app = express();
 
 // Connect to database
 db.connect();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'client')));
+app.use(express.static(path.join(__dirname, "client")));
 
-const outputFile = './services/swagger_output.json';
-const endpointsFiles = ['./routes/api.js'];
+const outputFile = "./services/swagger_output.json";
+const endpointsFiles = ["./routes/api.js"];
 const doc = {
     host: `localhost:${process.env.PORT}/api/v1`, // by default: 'localhost:3000'
 };
@@ -39,7 +39,7 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.locals.error = req.app.get("env") === "development" ? err : {};
 
     res.json({
         status: false,
