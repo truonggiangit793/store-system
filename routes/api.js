37e8506b686182550280910001f7bad807f227e0
TQ9Woaz9Lzp@HS <kjnwjnpham@gmail.com>
2022-10-24 16:47:14 +0700

    Get product

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,9 @@
 const Router = require("express").Router();
 const authorization = require("../middlewares/authorization");
+const authentication = require("../middlewares/authentication");
 const multer = require("../services/multer");
 
-const { login, importAccount, registerAccount } = require("./modules/accounts");
+const { login, importAccount, registerAccount, getProfile } = require("./modules/accounts");
 const { importSupplier, supplierRegister } = require("./modules/suppliers");
 const { productImport, productRegister } = require("./modules/products");
 
@@ -12,6 +13,8 @@ Router.post("/account/import", authorization.admin, multer.accountImport, import
 
 Router.post("/account/register", authorization.admin, registerAccount);
 
+Router.get("/account/me", authentication, getProfile);
+
 Router.post("/supplier/import", authorization.admin, multer.supplierImport, importSupplier);
 
 Router.post("/supplier/register", authorization.admin, supplierRegister);
@@ -19,5 +22,6 @@ Router.post("/supplier/register", authorization.admin, supplierRegister);
 Router.post("/products/import", authorization.admin, multer.productImport, productImport);
 
 Router.post("/products/register", authorization.admin, productRegister);
+
 
 module.exports = Router;
