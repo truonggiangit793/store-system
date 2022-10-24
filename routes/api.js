a88394c64095666698e0498bff825624db6a692c
truonggiangit793 <truonggiangit793@truonggiangit793-macbook.local>
2022-10-24 16:53:05 +0700

    API download example

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,12 @@
 const Router = require("express").Router();
 const authorization = require("../middlewares/authorization");
+const authentication = require("../middlewares/authentication");
 const multer = require("../services/multer");
 
 const { login, importAccount, registerAccount } = require("./modules/accounts");
 const { importSupplier, supplierRegister } = require("./modules/suppliers");
 const { productImport, productRegister } = require("./modules/products");
+const downloadExample = require("./modules/download_example");
 
 Router.post("/login", login);
 
@@ -19,5 +21,23 @@ Router.post("/supplier/register", authorization.admin, supplierRegister);
 Router.post("/products/import", authorization.admin, multer.productImport, productImport);
 
 Router.post("/products/register", authorization.admin, productRegister);
+
+Router.get(
+    "/file/download-example-account",
+    authentication,
+    downloadExample.downloadAccountExample
+);
+
+Router.get(
+    "/file/download-example-supplier",
+    authentication,
+    downloadExample.downloadSupplierExample
+);
+
+Router.get(
+    "/file/download-example-product",
+    authentication,
+    downloadExample.downloadProductExample
+);
 
 module.exports = Router;
